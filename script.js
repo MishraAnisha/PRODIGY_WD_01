62d17baa5fa6de38062279449129628835600423
Anisha Mishra <156603252+MishraAnisha@users.noreply.github.com>
2024-08-04 18:21:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+    document.getElementById("nav_mob")
+        .style.width = "50%";
+}
+function closeNavbar() {
+    document.getElementById("nav_mob")
+        .style.width = "0%";
+}
